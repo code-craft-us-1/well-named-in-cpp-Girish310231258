23cc5b4349fbcf99a298466326e175ef22d1feeb
Girish310231258 <girish.kumar.p@philips.com>
2024-09-10 10:41:27 +0530

    wellnamed7

--- main.cpp
+++ main.cpp
@@ -26,14 +26,18 @@ void testPairToNumber(TelCoColorCoder::MajorColor major,
 	std::cout << "Got pair number " << pairNumber << '\n';
 	assert(pairNumber == expectedPairNumber);
 }
-
-int main()
+void UnitTests()
 {
 	testNumberToPair(4, TelCoColorCoder::MajorColor::WHITE, TelCoColorCoder::MinorColor::BROWN);
 	testNumberToPair(5, TelCoColorCoder::MajorColor::WHITE, TelCoColorCoder::MinorColor::SLATE);
 
 	testPairToNumber(TelCoColorCoder::MajorColor::BLACK, TelCoColorCoder::MinorColor::ORANGE, 12);
 	testPairToNumber(TelCoColorCoder::MajorColor::VIOLET, TelCoColorCoder::MinorColor::SLATE, 25);
+}
+
+int main()
+{
+	UnitTests();
 
 	TelCoColorCoder::ColorPair::PrintReferenceManual();
 
